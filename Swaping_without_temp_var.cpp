1e1f1a6e8ce65dbbbdd96d24698b4c4511a7e9f1
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2021-02-23 20:37:32 +0600

    Update Swaping_without_temp_var.cpp

--- Swaping_without_temp_var.cpp
+++ Swaping_without_temp_var.cpp
@@ -1,1 +1,15 @@
+///******* In the name of Allah *******///
 
+include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int a = 7, b = 6;
+   a += b;
+   b = a - b;
+   a -= b;
+   cout<<a<<" "<<b<<endl;
+
+   return 0;
+}
